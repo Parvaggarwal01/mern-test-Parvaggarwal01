c5c81a903fda5a17ae80e4cdbae21e9a44419855
Parvaggarawal01 <parvaggarwal130@gmail.com>
2026-02-27 14:58:53 +0530

    fixed mongoose connection string

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/tasks'));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error(err));
 
